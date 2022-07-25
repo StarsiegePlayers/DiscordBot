2b66e758b2d4a12429351ed10f553ac921428271
Neo <321592+Neo-Desktop@users.noreply.github.com>
2022-07-25 02:09:40 -0700

    Add OwO command/handler/dependencies
    	modified:   discord/handlers.go
    	modified:   go.mod
    	modified:   go.sum

--- discord/handlers.go
+++ discord/handlers.go
@@ -6,7 +6,14 @@ import (
 	"strings"
 
 	"golang.org/x/exp/slices"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+
+	"github.com/bwmarrin/discordgo"
+	owo "github.com/deadshot465/owoify-go/v2"
 )
+
+const DefaultColor = 0xff88ff
 
 func (s *Service) registerHandlers() {
 	s.commands.Register(Command{
@@ -27,13 +34,47 @@ func (s *Service) registerHandlers() {
 		Summary: "Sends a DM to the user with a message validating the bot is online.",
 		Usage:   "ping",
 	})
+	s.commands.Register(Command{
+		Name:    "owo",
+		Handler: s.owoHandler,
+		Summary: "Turns ywour text intwo swomwething owo-ifwied.",
+		Usage:   "owo <text>",
+	})
+	s.commands.Register(Command{
+		Name:    "uwu",
+		Handler: s.owoHandler,
+		Summary: "Tuwns ywouw text intwo swomwefwing owo-ifwied(o･ω･o).",
+		Usage:   "uwu <text>",
+	})
+	s.commands.Register(Command{
+		Name:    "uvu",
+		Handler: s.owoHandler,
+		Summary: "Tuwns ywowouw text indwowo swowomwefwing owowowo-ifwieduwu.",
+		Usage:   "uvu <text>",
+	})
 
 	s.registerAdminHandlers()
 	s.registerStarsiegeHandlers()
 }
 
 func (s *Service) commandsHandler(d *Session, m *MessageCreate, payload string) {
-	options := strings.Split(payload, " ")
+	var (
+		err                  error
+		fields               []*discordgo.MessageEmbedField
+		titleCaseCommand     = []byte(m.Command.Name)
+		titleCaseTransformer = cases.Title(language.English)
+		options              = strings.Split(payload, " ")
+		nick                 = m.Member.Nick
+	)
+
+	if nick == "" {
+		nick = m.Author.Username
+	}
+
+	_, _, err = titleCaseTransformer.Transform(titleCaseCommand, []byte(m.Command.Name), true)
+	if err != nil {
+		s.Logln("error transforming command name to title case:", err)
+	}
 
 	if len(options) == 1 && options[0] == "" {
 		var commands []string
@@ -66,11 +107,20 @@ func (s *Service) commandsHandler(d *Session, m *MessageCreate, payload string) 
 		}
 
 		sort.Strings(commands)
-		for i, v := range commands {
-			commands[i] = fmt.Sprintf("`%s`", v)
+		for _, v := range commands {
+			fields = append(fields, &discordgo.MessageEmbedField{
+				Name:  fmt.Sprintf("%s%s", d.GuildConfig.CommandPrefix, s.commands[v].Usage),
+				Value: s.commands[v].Summary,
+			})
 		}
 
-		_, err := d.ChannelMessageMentionSend(m.ChannelID, m.Author, fmt.Sprintf("Available commands: %s", strings.Join(commands, ", ")))
+		embed := &discordgo.MessageEmbed{
+			Title:  fmt.Sprintf("%s | %s", titleCaseCommand, nick),
+			Color:  DefaultColor,
+			Fields: fields,
+		}
+
+		_, err := d.ChannelMessageSendEmbed(m.ChannelID, embed)
 		if err != nil {
 			s.Logln("error sending commands message:", err)
 			return
@@ -79,22 +129,44 @@ func (s *Service) commandsHandler(d *Session, m *MessageCreate, payload string) 
 	}
 
 	if cmd, exists := s.commands[options[0]]; exists {
-		_, err := d.ChannelMessageMentionSend(m.ChannelID, m.Author, fmt.Sprintf("[%s] %s", cmd.Name, cmd.Summary))
+		fields = []*discordgo.MessageEmbedField{
+			{
+				Name:  fmt.Sprintf("%s%s", d.GuildConfig.CommandPrefix, cmd.Usage),
+				Value: cmd.Summary,
+			},
+		}
+
+		embed := &discordgo.MessageEmbed{
+			Title:  fmt.Sprintf("%s | %s", titleCaseCommand, nick),
+			Color:  DefaultColor,
+			Fields: fields,
+		}
+
+		_, err := d.ChannelMessageSendEmbed(m.ChannelID, embed)
 		if err != nil {
-			s.Logln("error sending specific command summary message:", err)
+			s.Logln("error sending commands message:", err)
 			return
 		}
-		_, err = d.ChannelMessageMentionSend(m.ChannelID, m.Author, fmt.Sprintf("[%s] %s", cmd.Name, s.formatUsageMessage(d.GuildConfig.CommandPrefix, cmd.Usage)))
-		if err != nil {
-			s.Logln("error sending specific command usage message:", err)
-			return
-		}
+
 		return
 	}
 
-	_, err := d.ChannelMessageMentionSend(m.ChannelID, m.Author, fmt.Sprintf("Unknown command: %s", options[0]))
+	fields = []*discordgo.MessageEmbedField{
+		{
+			Name:  fmt.Sprintf("Unknown command: %s", options[0]),
+			Value: fmt.Sprintf("Try `%s%s` for a list of commands.", d.GuildConfig.CommandPrefix, m.Command.Name),
+		},
+	}
+
+	embed := &discordgo.MessageEmbed{
+		Title:  fmt.Sprintf("%s | %s", titleCaseCommand, nick),
+		Color:  DefaultColor,
+		Fields: fields,
+	}
+
+	_, err = d.ChannelMessageSendEmbed(m.ChannelID, embed)
 	if err != nil {
-		s.Logln("error sending unknown command message:", err)
+		s.Logln("error sending commands message:", err)
 		return
 	}
 }
@@ -109,5 +181,32 @@ func (s *Service) pingHandler(d *Session, m *MessageCreate, payload string) {
 	_, err = d.ChannelMessageSend(dm.ID, "Pong!")
 	if err != nil {
 		s.Logln("error sending DM message:", err)
+	}
+}
+
+func (s *Service) owoHandler(d *Session, m *MessageCreate, payload string) {
+	var (
+		owoness owo.Owoness
+		output  string
+	)
+
+	switch m.Command.Name {
+	case "owo":
+		owoness = owo.Owo
+	case "uwu":
+		owoness = owo.Uwu
+	case "uvu":
+		owoness = owo.Uvu
+	}
+
+	if len(payload) == 0 {
+		payload = fmt.Sprintf("error: please specify some text to %s", m.Command.Name)
+	}
+
+	output = owo.Owoify(payload, owoness)
+
+	_, err := d.ChannelMessageMentionSend(m.ChannelID, m.Author, output)
+	if err != nil {
+		s.Logln("error sending message:", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,18 +3,18 @@ module github.com/StarsiegePlayers/DiscordBot
 go 1.18
 
 require (
-	github.com/Clinet/discordgo-embed v0.0.0-20220113222025-bafe0c917646
 	github.com/ThreeDotsLabs/watermill v1.1.1
 	github.com/bwmarrin/discordgo v0.25.0
+	golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d
 	google.golang.org/api v0.77.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	cloud.google.com/go/compute v1.6.1 // indirect
+	github.com/deadshot465/owoify-go/v2 v2.0.1 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
@@ -23,7 +23,6 @@ require (
 	github.com/pkg/errors v0.8.1 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
-	golang.org/x/exp v0.0.0-20220613132600-b0d781184e0d // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
 	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
